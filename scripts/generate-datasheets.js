18fcd731fa0e1e6a23241ad6dfc74dc1d5890a7a
Sebastian Romero <s.hunkeler@arduino.cc>
2021-03-08 12:42:03 +0100

    Add scaffold for PDF generator

--- scripts/generate-datasheets.js
+++ scripts/generate-datasheets.js
@@ -1,6 +1,13 @@
 const fileHelper = require('./file-helper');
+const parser = require("./datasheet-generator/parser"); //TODO turn parser into a module
 
 let datasheetFiles = fileHelper.findAllFiles("content/datasheets/", ".md");
+
+for(let filePath of datasheetFiles){
+    console.log(filePath);
+    //TODO
+    //parser.generatePDF(filePath);
+}
 
 console.log("✅ %s Datasheets generated.", datasheetFiles.length);
 process.exit(0);
